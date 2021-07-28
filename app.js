cf903232833d11b137e07e29e41923f5fd290527
= <=>
2021-07-28 14:21:12 +0800

    app.js console

--- app.js
+++ app.js
@@ -4,5 +4,7 @@
  * @Author: 曾利锋[阿牛]
  * @Date: 2021-07-28 13:58:10
  * @LastEditors: 曾利锋[阿牛]
- * @LastEditTime: 2021-07-28 13:58:11
+ * @LastEditTime: 2021-07-28 14:20:37
  */
+
+ console.log("cci")
